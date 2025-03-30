ff13a736994303b2db19f9406e149c32dd59c91c
jaderAdriel <jaderadriel7@gmail.com>
2025-03-30 04:02:29 -0300

    refactor: improve mobile menu functionality and accessibility

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@
 const buttonMenu = document.getElementById("menu-button");
 const menu = document.getElementById("page-tabs");
 const navbar = document.getElementById("navbar");
+
+const openMenu = () => {
+    buttonMenu.querySelector("img").src = "assets/icons/close-svgrepo-com.svg";
+    buttonMenu.classList.remove("closed");
+    buttonMenu.setAttribute("aria-expanded", "true");
+    menu.classList.remove("hidden");
+    navbar.classList.add("menu-open-mobile");
+}
+
+const closeMenu = () => {
+    buttonMenu.querySelector("img").src = "assets/icons/menu-svgrepo-com.svg";
+    buttonMenu.classList.add("closed");
+    buttonMenu.setAttribute("aria-expanded", "false");
+    menu.classList.add("hidden");
+    navbar.classList.remove("menu-open-mobile");
+}
+
 buttonMenu.addEventListener("click", () => {
 
     if (buttonMenu.classList.contains("closed")) {
@@ -57,21 +74,7 @@ document.getElementById('load-map-btn').addEventListener('click', function() {
 });
 
 
-const openMenu = () => {
-    buttonMenu.querySelector("img").src = "assets/icons/close-svgrepo-com.svg";
-    buttonMenu.classList.remove("closed");
-    buttonMenu.setAttribute("aria-expanded", "true");
-    menu.classList.remove("hidden");
-    navbar.classList.add("menu-open-mobile");
-}
 
-const closeMenu = () => {
-    buttonMenu.querySelector("img").src = "assets/icons/menu-svgrepo-com.svg";
-    buttonMenu.classList.add("closed");
-    buttonMenu.setAttribute("aria-expanded", "false");
-    menu.classList.add("hidden");
-    navbar.classList.remove("menu-open-mobile");
-}
 
 const locationMapUrl = new Map();
 locationMapUrl.set("São Paulo", "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.1525542549803!2d-46.58714562373215!3d-23.562963861652886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5eaf6988f685%3A0x4fec87ce3b087a87!2sDon%20Contabil!5e0!3m2!1sen!2sbr!4v1743132457475!5m2!1sen!2sbr" );
